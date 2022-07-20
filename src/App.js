a216b7eb69d0011268da66e3e006c5b0f2f71481
samvelafrikyan <samvelafrikyan2000@gmail.com>
2022-07-20 19:24:20 +0400

    deployed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
+import React from 'react';
+import Navbar from './components/Navbar';
 import './App.css';
+import Home from './components/pages/Home';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Services from './components/pages/Services';
+import ContactUs from './components/pages/ContactUs';
+import SignUp from './components/pages/SignUp';
+import Marketing from './components/pages/Marketing';
+import Consulting from './components/pages/Consulting';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path='/' exact element={<Home/>} />
+        <Route path='/services' element={<Services/>} />
+        <Route path='/contact-us' element={<ContactUs/>} />
+        <Route path='/sign-up' element={<SignUp/>} />
+        <Route path='/marketing' element={<Marketing/>} />
+        <Route path='/consulting' element={<Consulting/>} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/components/MenuItems.js
@@ -0,0 +1,23 @@
+export let MenuItems = [
+    {
+        title: 'Marketing',
+        path: "/marketing",
+        cName: "dropdown-link"
+    },
+    {
+        title: 'Consulting',
+        path: "/consulting",
+        cName: "dropdown-link"
+    },
+    {
+        title: 'Design',
+        path: "/design",
+        cName: "dropdown-link"
+    },
+    {
+        title: 'Development',
+        path: "/development",
+        cName: "dropdown-link"
+    }
+
+];
